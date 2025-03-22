98d2f0287c8ea28479c4aa3ea1e7250dc343bd22
Nic <130799100+SpazticSqurrel@users.noreply.github.com>
2025-03-22 07:42:24 -0500

    import door&mallow, modify level_door and Box

--- assets/level_editor/level_door_area_3d.gd
+++ assets/level_editor/level_door_area_3d.gd
@@ -1,7 +1,7 @@
 extends Area3D
 
 func _on_body_entered(body: Node3D) -> void:
-	$"../AnimationPlayer".play("open")
+	$"../AnimationPlayer".play("Open")
 
 func _on_body_exited(body: Node3D) -> void:
-	$"../AnimationPlayer".play_backwards("open")
+	$"../AnimationPlayer".play_backwards("Open")
