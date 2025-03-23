bed3e568a31fb3f02c4d32eafa7bfd9c201e9b11
Norden-Tenzin <nordentenzin29@gmail.com>
2025-03-23 19:59:14 +0530

    added dialogic addon

--- utils/autoloads/global.gd
+++ utils/autoloads/global.gd
@@ -10,11 +10,5 @@ var music_volume: float = 1.0
 var sfx_volume: float = 1.0
 
 # TODO: 
-# - player candy relation - DONE
-# - add handleing for pressure plate - DONE
-# - connect pin wheel to fan - DONE
-# - witch billboard, kid billboard - DONE
-# - dialog intro - DONE
-# - candy transition - DONE
-# - shaders - 
-# - fix bug
+# - menu window
+# - candy collision
