ce9ec0f4b75758b5035639daf344be661d73d37a
Norden-Tenzin <nordentenzin29@gmail.com>
2025-03-23 14:48:45 +0530

    fixed the pressure plate visual bug

--- scenes/pressure_plate/pressure_plate.gd
+++ scenes/pressure_plate/pressure_plate.gd
@@ -15,7 +15,10 @@ func _ready() -> void:
 	glow = frame.mesh.surface_get_material(1)
 
 func _process(delta: float) -> void:
-	update_visuals(delta * 3.0)
+	if plate_state:
+		update_visuals_on(delta * 3.0)
+	else:
+		update_visuals_off(delta * 3.0)
 
 func _on_area_component_node_entered(body: Node3D) -> void:
 	match plate_type:
@@ -35,6 +38,10 @@ func _on_area_component_node_exited(body: Node3D) -> void:
 	plate_state_changed.emit(self.get_instance_id(), plate_state)
 	debug_label.text = str(plate_state)
 
-func update_visuals(lerp_alpha: float) -> void:
-	glow.set_shader_parameter("intensity", lerp(glow.get_shader_parameter("intensity"), 3.0 if plate_state else 0.0, lerp_alpha / 2.0))
+func update_visuals_on(lerp_alpha: float) -> void:
+	glow.set_shader_parameter("intensity", lerp(glow.get_shader_parameter("intensity"), 3.0, lerp_alpha / 2.0))
+	button.scale.y = lerp(button.scale.y, 0.2 if plate_state else 1.0, lerp_alpha)
+
+func update_visuals_off(lerp_alpha: float) -> void:
+	glow.set_shader_parameter("intensity", lerp(glow.get_shader_parameter("intensity"), -3.0, lerp_alpha / 2.0))
 	button.scale.y = lerp(button.scale.y, 0.2 if plate_state else 1.0, lerp_alpha)
